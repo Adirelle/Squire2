434e4d62b0575330270e7502c81bb7ff851d03df
Adirelle <adirelle@tagada-team.net>
2010-10-11 15:00:27 +0200

    Got rid of AceAddon, AceEvent and AceHook. Added configuration panel.

--- Localization.lua
+++ Localization.lua
@@ -4,20 +4,20 @@ Copyright 2010 Adirelle (adirelle@tagada-team.net)
 All rights reserved.
 --]]
 
-local addonName, ns = ...
+local addonName, addon = ...
 
 local L = setmetatable({}, {
 	__index = function(self, key)
 		if key ~= nil then
 			--@debug@
-			ns.Debug('Missing locale', tostring(key))
+			addon.Debug('Missing locale', tostring(key))
 			--@end-debug@
 			rawset(self, key, tostring(key))
 		end
 		return tostring(key)
 	end,
 })
-ns.L = L
+addon.L = L
 
 --------------------------------------------------------------------------------
 -- Locales from localization system
--- Squire2_Config.lua
+++ Squire2_Config.lua
@@ -4,13 +4,20 @@ Copyright 2010 Adirelle (adirelle@tagada-team.net)
 All rights reserved.
 --]]
 
-local addon = LibStub("AceAddon-3.0"):GetAddon("Squire2")
+local addon = Squire2
 local L, Debug = addon.L, addon.Debug
 
+local AceConfigDialog = LibStub('AceConfigDialog-3.0')
+
 local checkbuttons = {}
+local panelButton
+
 local CheckButton_OnClick, CheckButton_OnEnter, CheckButton_OnLeave
 
+local MACRO_NAME, MACRO_ICON = "Squire2", 1
+
 function addon:InitializeConfig()
+	
 	for i = 1, NUM_COMPANIONS_PER_PAGE do
 		local button = _G["SpellBookCompanionButton"..i];
 		local checkbutton = CreateFrame("CheckButton", nil, button, "UICheckButtonTemplate")
@@ -21,6 +28,16 @@ function addon:InitializeConfig()
 		checkbutton:SetScript('OnLeave', CheckButton_OnLeave)
 		checkbuttons[i] = checkbutton
 	end
+	
+	LibStub('AceConfig-3.0'):RegisterOptionsTable("Squire2", addon.GetOptions)
+
+	panelButton = CreateFrame("Button", nil, SpellBookCompanionsFrame, "UIPanelButtonTemplate")
+	panelButton:SetText("Squire2")
+	panelButton:SetPoint("RIGHT", SpellBookFrameCloseButton, "LEFT", 4, 0)
+	panelButton:SetSize(70, 22)
+	panelButton:Hide()
+	panelButton:SetScript('OnClick', function() AceConfigDialog:Open("Squire2") end)
+	
 	return self:SpellBook_UpdateCompanionsFrame()
 end
 
@@ -29,6 +46,7 @@ function addon:SpellBook_UpdateCompanionsFrame()
 		for i, checkbutton in pairs(checkbuttons) do
 			checkbutton:Hide()
 		end
+		panelButton:Hide()
 		return
 	end
 	for i, checkbutton in ipairs(checkbuttons) do
@@ -41,26 +59,103 @@ function addon:SpellBook_UpdateCompanionsFrame()
 			checkbutton:Hide()
 		end
 	end
+	panelButton:Show()
 end
 
-function CheckButton_OnClick(checkbutton)
-	local id = tonumber(checkbutton:GetParent().spellID)
+--------------------------------------------------------------------------------
+-- Handlers of the mount checkbuttons
+--------------------------------------------------------------------------------
+
+function CheckButton_OnClick(self)
+	local id = tonumber(self:GetParent().spellID)
 	Debug("Click checkbutton", i, id, id and addon.db.char.mounts[id])
 	if id then
 		addon.db.char.mounts[id] = not addon.db.char.mounts[id]
 	end
 end
 
-function CheckButton_OnEnter(checkbutton)
-	GameTooltip:SetOwner(checkbutton, "ANCHOR_BOTTOMLEFT")
+function CheckButton_OnEnter(self)
+	GameTooltip:SetOwner(self, "ANCHOR_BOTTOMLEFT")
 	GameTooltip:ClearLines()
-	GameTooltip:AddLine(format(L["Use %s"], GetSpellInfo(checkbutton:GetParent().spellID)),1,1,1)
+	GameTooltip:AddLine(format(L["Use %s"], GetSpellInfo(self:GetParent().spellID)),1,1,1)
 	GameTooltip:AddLine(L["Check this to let Squire2 use this mount."], 0.1, 1, 0.1)
 	GameTooltip:Show()
 end
 
-function CheckButton_OnLeave(checkbutton)
-	if GameTooltip:GetOwner() == checkbutton then
+function CheckButton_OnLeave(self)
+	if GameTooltip:GetOwner() == self then
 		GameTooltip:FadeOut()
 	end
 end
+
+--------------------------------------------------------------------------------
+-- Handlers of the panel button
+--------------------------------------------------------------------------------
+
+function PanelButton_OnHide(self)
+	self:SetText("Squire2 >>")
+	if self.panel then
+		self.panel:Release()
+		self.panel = nil
+	end
+end
+
+--------------------------------------------------------------------------------
+-- Options
+--------------------------------------------------------------------------------
+
+local options
+function addon.GetOptions()
+	if not options then
+		options = {
+			name = "Squire2",
+			type = "group",
+			args = {
+				macro = {
+					name = L['Macro'],
+					desc = L['Create and pick up the Squire2 macro'],
+					type = 'execute',
+					func = function()
+						local index = GetMacroIndexByName(MACRO_NAME)
+						if index == 0 then
+							index = CreateMacro(MACRO_NAME, MACRO_ICON, "/click Squire2Button", 0)
+						end
+						PickupMacro(index)
+					end,
+					order = 10,
+				},
+				autoDismount = {
+					name = L['Dismount automatically'],
+					desc = L['Check this to have Squire2 automatically dismount you.'],
+					type = 'toggle',
+					get = function() return addon.db.profile.autoDismount end,
+					set = function(_, value) addon.db.profile.autoDismount = value end,
+					order = 20,
+				},
+				safeDismount = {
+					name = L['Safe dismount'],
+					desc = L['Check this to prevent Squire2 from automatically dismounting you when you are flying.'],
+					type = 'toggle',
+					get = function() return addon.db.profile.safeDismount end,
+					set = function(_, value) addon.db.profile.safeDismount = value end,
+					disabled = function() return not addon.db.profile.autoDismount end,
+					order = 30,
+				},
+				combatAction = {
+					name = L['Combat action'],
+					desc = L['Drag an drop any action there to define the action to do it in combat instead of mounting.'],
+					type = 'input',
+					control = 'ActionSlot',
+					order = 40,
+					get = function() return addon.db.char.combatAction end,
+					set = function(_, value) addon.db.char.combatAction = value end,
+					validate = function(_, value)
+						return (value ~= "macro:Squire2") or L["Infinite recursion is bad"]
+					end
+				},
+			}
+		}
+	end
+	return options
+end
+
